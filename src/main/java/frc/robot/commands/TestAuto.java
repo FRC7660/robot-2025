c0c2ed5bdc70e5adb0a55750d2ce94640f02ecfc
AlexaDaniels25 <adaniels@fgrhs.org>
2025-01-28 18:41:50 -0500

    merge with main

--- src/main/java/frc/robot/commands/TestAuto.java
+++ src/main/java/frc/robot/commands/TestAuto.java
@@ -4,16 +4,13 @@ package frc.robot.commands;
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-
-
-//import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class TestAuto extends Command {
   /** Creates a new Test. */
   private String name;
-
 
   public TestAuto(String nameInput) {
     // Use addRequirements() here to declare subsystem dependencies.
@@ -24,7 +21,7 @@ public class TestAuto extends Command {
   @Override
   public void initialize() {
     System.out.println(name + " is Initialized");
-    //SmartDashboard.putNumber(getName(), 2);
+    // SmartDashboard.putNumber(getName(), 2);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
