ff290ec018e3134dcdd4208fa3cefe53bd67064f
karmikhael <solomoncarmichael@gmail.com>
2025-02-22 17:37:39 -0500

    Reads pose3d thangs from da json string - does not do dem networktables swtcharoosies thang

--- src/main/java/frc/robot/subsystems/VisionReader.java
+++ src/main/java/frc/robot/subsystems/VisionReader.java
@@ -4,28 +4,65 @@
 
 package frc.robot.subsystems;
 
+
+import java.util.HashMap;
+
+// import org.json.simple.JSONArray;
+// import org.json.simple.JSONObject;
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Translation3d;
+
+
+
+
 
 public class VisionReader extends SubsystemBase {
   /** Creates a new VisionReader. */
-  public VisionReader() {
-    // ObjectMapper objectMapper = new ObjectMapper();
-
-    // String jsonString = jsonEntry.getString("{}");
-
-    // @SuppressWarnings("unchecked")
-    // Map<String, Object> map = objectMapper.readValue(jsonString, Map.class);
-    // System.out.println(map); // Output: {name=Alice, age=28}
-    // } catch (Exception e) {
-    // e.printStackTrace();
-    // }
+public VisionReader() {
+    
   }
+
+ 
 
   @Override
   public void periodic() {
-    String dataThing = SmartDashboard.getString("detecTable", "{}");
-    System.out.println(dataThing);
-    // This method will be called once per scheduler run
+   
+
+    String fromTable = SmartDashboard.getString("detecTable", "[]");
+
+    System.out.println(fromTable);
+
+    JSONArray top = new JSONArray(fromTable);
+    int x = top.length();
+    System.out.println(x);
+    if(x>0){
+      JSONObject dict = (JSONObject)top.get(0);
+      System.out.println(dict);
+    }
+    // String pageName = obj.getJSONObject("pageInfo").getString("pageName");
+    // String pageName = obj.getString("tag_family");
+    // JSONObject jsonThing = top.getJSONObject(0);
+    // // System.out.println(jsonThing);
+
+    // JSONArray pose_R = jsonThing.getJSONArray("pose_R");
+    // JSONArray pose_t = jsonThing.getJSONArray("pose_t");
+    // int tag_id = jsonThing.getInt("tag_id");
+    // //Int tag_Id = new Int(jsonThing.getInt("tag_id"));
+    // System.out.println(pose_R);
+
+    
+    
+
   }
 }
