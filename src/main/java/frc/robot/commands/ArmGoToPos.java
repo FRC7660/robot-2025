4631490b7389076b6fb0651ec89aa6d69202d61c
robo7660 <robo7660@github.com>
2025-03-11 15:59:03 -0700

    added danger zones for arm

--- src/main/java/frc/robot/commands/ArmGoToPos.java
+++ src/main/java/frc/robot/commands/ArmGoToPos.java
@@ -5,24 +5,48 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
 import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.elevator.Elevator;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class ArmGoToPos extends Command {
-  private final Arm arm;
+  private final Arm m_arm;
+  private final Elevator m_elevator;
   private final double targetPos;
+  private double limitOut, limitIn;
 
   /** Creates a new ArmGoToPos. */
-  public ArmGoToPos(Arm arm, double Pos) { // ADD ELEVATOR TO REQUIREMENTS
+  public ArmGoToPos(Arm arm, Elevator elevator, double Pos) { // ADD ELEVATOR TO REQUIREMENTS
     // Use addRequirements() here to declare subsystem dependencies.
-    this.arm = arm;
+    m_arm = arm;
     targetPos = Pos;
+    m_elevator = elevator;
+    addRequirements(m_arm, m_elevator);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    arm.setTarget(targetPos);
+    m_elevator.hold();
+
+    if (!m_elevator.isAtBottom()) {
+      limitIn = Constants.Arm.safePosIn;
+      limitOut = Constants.Arm.safePosOut;
+    } else {
+      limitIn = Constants.Arm.zeroPos;
+      limitOut = Constants.Arm.forewardLimit;
+    }
+
+    if (targetPos > limitIn) {
+      cancel();
+    }
+
+    if (targetPos < limitOut) {
+      cancel();
+    }
+
+    m_arm.setTarget(targetPos);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -38,6 +62,6 @@ public class ArmGoToPos extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return arm.atTargetPos();
+    return m_arm.atTargetPos();
   }
 }
