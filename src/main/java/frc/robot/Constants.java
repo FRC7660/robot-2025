d0d66195ee8d72458026c7eda75ff6ef412534be
robo7660 <robo7660@github.com>
2025-03-12 16:29:40 -0700

    climb-constant-change

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -87,7 +87,7 @@ public final class Constants {
     public static double climbSpeed = 0.50;
     public static double climbEncoderLimit = 1;
     public static int climbSwitchID = 5;
-    public static double upperLimit = 165;
+    public static double upperLimit = 270;
     public static double halfwayPosition = 80;
   }
 
