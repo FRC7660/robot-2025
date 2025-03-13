0787a4e635b0f8aabb7b497ea2aa4e7ef87ca16f
robo7660 <robo7660@github.com>
2025-03-13 15:08:02 -0700

    Elevator Manual rewritten to be like Arm

--- src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -181,6 +181,7 @@ public class Elevator extends SubsystemBase {
         break;
     }
 
+    m_controller.reset(getPosition());
     m_controller.setGoal(goal);
     manual = false;
   }
@@ -210,11 +211,6 @@ public class Elevator extends SubsystemBase {
     SmartDashboard.putNumber("Motor Alpha Speed", motorAlpha.get());
     SmartDashboard.putNumber("Motor Alpha Position", motorAlphaEncoder.getPosition());
 
-    if (manual) {
-      setVoltage(manualOutput);
-      return;
-    }
-
     if (debug) {
       SmartDashboard.putBoolean("Elevator Limit Reached", !bottomLimit.get());
       SmartDashboard.putNumber("Alpha Applied", motorAlpha.getAppliedOutput());
@@ -243,7 +239,12 @@ public class Elevator extends SubsystemBase {
     SmartDashboard.putNumber("PID feedForward calculation", feedForward);
     SmartDashboard.putNumber("PID Goal", m_controller.getGoal().position);
     SmartDashboard.putNumber("Elevator Counter", counter);
-    setCalculatedMotors(output, feedForward);
+
+    if (manual) {
+      setVoltage(manualOutput);
+    } else {
+      setCalculatedMotors(output, feedForward);
+    }
   }
 
   public void simulationPeriodic() {
